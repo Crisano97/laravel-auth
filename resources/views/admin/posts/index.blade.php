8337d1746d01c8759c0e34e4bb3d5bb95e02116b
Crisano97 <97keb6@gmail.com>
2022-09-19 17:49:04 +0200

    showing posts table

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>index</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+
+            <table class="table table-striped">
+                <thead>
+                    <th>ID</th>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @forelse ($posts as $post)
+                        <tr>
+                            <td>{{ $post->id }}</td>
+                            <td>
+                                <a href="#">
+                                    {{ $post->author }}    
+                                </a>
+                            </td>
+                            <td>{{ $post->title }}</td>
+                            <td>
+                                <a href="" class="btn btn-sm btn-success">
+                                    Edit
+                                </a>
+                                <a href="" class="btn btn-sm btn-danger">
+                                    Delete
+                                </a>
+                            </td>
+                            
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan=7>non sono disponibili post</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
